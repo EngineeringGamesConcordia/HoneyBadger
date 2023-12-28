0e7370b2459a6b1e7c97f3bcf4e2e19357992adc
VPMachine <37657399+Xeneton@users.noreply.github.com>
2023-12-28 05:11:49 -0500

    test

--- Badger/arm.py
+++ Badger/arm.py
@@ -20,6 +20,8 @@ def calculate_inverse_kinematic(px, py):
 
     #lengths of the arm are 22
     arm = tinyik.Actuator(['z', [22., 0., 0.], 'z', [22., 0., 0.]])
+    theta_1 = 0
+    theta_2 = 0
 
     ikangles = [theta_1, theta_2]
     arm.ee = [px, py]
