337261197d84b8490bf4bb5bb500541731309984
VPMachine <37657399+Xeneton@users.noreply.github.com>
2023-12-28 19:43:07 -0500

    test

--- Badger/arm.py
+++ Badger/arm.py
@@ -57,7 +57,8 @@ def calculate_inverse_kinematic(x_target, y_target):
     
     if np.abs(D) > 1:
         print("No solution for given x, y.")
-        return np.rad2deg(theta), 0, cost
+        cost = 0
+        return initial_theta1, initial_theta2, cost
     
     theta2_1 = np.arctan2(np.sqrt(1 - D**2), D)
     theta2_2 = -np.arctan2(np.sqrt(1 - D**2), D)
