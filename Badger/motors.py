ae954de7729742f696772efb4018bebe1465285e
MathiasAavaa <138600494+MathiasAavaa@users.noreply.github.com>
2023-12-28 20:49:56 -0500

    speed control

--- Badger/motors.py
+++ Badger/motors.py
@@ -1,4 +1,3 @@
-
 import RPi.GPIO as GPIO
 from time import sleep
 
@@ -8,21 +7,22 @@ class dcMotor:
         self.bpwm = bpwm
         GPIO.setup(self.fpwm, GPIO.OUT)
         GPIO.setup(self.bpwm, GPIO.OUT)
-        GPIO.output(self.fpwm, GPIO.LOW)
-        GPIO.output(self.bpwm, GPIO.LOW)
-        
+        self.pwm_forward = GPIO.PWM(self.fpwm, 1000)  # 1000 Hz frequency
+        self.pwm_backward = GPIO.PWM(self.bpwm, 1000)
+        self.pwm_forward.start(0)  # Start with 0% duty cycle
+        self.pwm_backward.start(0)
 
-    def cw(self):
-        GPIO.output(self.bpwm, GPIO.LOW)
-        GPIO.output(self.fpwm, GPIO.HIGH)
+    def cw(self, duty_cycle):
+        self.pwm_backward.ChangeDutyCycle(0)  # Ensure backward PWM is off
+        self.pwm_forward.ChangeDutyCycle(duty_cycle* 100) # from 0 to 1
 
-    def ccw(self):
-        GPIO.output(self.fpwm, GPIO.LOW)
-        GPIO.output(self.bpwm, GPIO.HIGH)
+    def ccw(self, duty_cycle):
+        self.pwm_forward.ChangeDutyCycle(0)  # Ensure forward PWM is off
+        self.pwm_backward.ChangeDutyCycle(duty_cycle* 100) 
 
     def stop(self):
-        GPIO.output(self.fpwm, GPIO.LOW)
-        GPIO.output(self.bpwm, GPIO.LOW)
+        self.pwm_forward.ChangeDutyCycle(0)
+        self.pwm_backward.ChangeDutyCycle(0)
 
         #code below to get rid of
 class stepperMotor:
