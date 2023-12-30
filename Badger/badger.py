52e10c974449b7532145eb31df1c672cb88811f4
cam12348035 <camgranade@gmail.com>
2023-12-29 21:32:05 -0500

    Badger automation script done without right time

--- Badger/badger.py
+++ Badger/badger.py
@@ -23,6 +23,10 @@ def threadFunction(controller):
     print("Spawning controller thread")
     controller.listen()  # set on_disconnect=restart for final usage
 
+def automation_begin(automation): 
+    print("Starting automatic script)
+    automation.start(arm)
+
 GPIO.setmode(GPIO.BCM)
 
 #TODO test and fix parameters
@@ -47,7 +51,8 @@ def ticks():#works
 try:
     controller = BadgerController(arm1, drivesys, vacuum1, arm1.wrist_r_servo, automation1, interface="/dev/input/js0", connecting_using_ds4drv=False)
     t1 = threading.Thread(target=threadFunction, args=(controller,))
-    t2 =  threading.Thread(target=ticks)
+    t2 = threading.Thread(target=ticks)
+#    t3 = threading.Thread(target=automation_begin, args(automation, ))
 
     t1.start() 
     t2.start()
